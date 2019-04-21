ed9e0981df8e9a384729650646dc0d8cbb844e23
NickNIck <40608211+chemplooo@users.noreply.github.com>
2019-04-21 17:11:05 +0300

    LessonTWO

--- LessonOne/js/script.js
+++ LessonOne/js/script.js
@@ -1,5 +1,5 @@
 let num = '33721';
-for(var i=1,acc=num[0]; i< num.length;i++){
+fo r(var i=1,acc=num[0]; i< num.length;i++){
   console.log(acc*=num[i]) 
 }
 console.log(acc=acc**3) 
---
+++ LessonTWO/js/script.js
@@ -0,0 +1,27 @@
+let week = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'],
+    toDay = new Date().getDay() - 1;
+
+    for (let i = 0; i < week.length; i++) {
+      if (i == toDay) {
+        if (week[i] == 'сб' || week[i] == 'вс') {
+          document.write(`<p><b><i>${week[i]}</i></b></p>`);
+        } else {
+          document.write(`<p><b>${week[i]}</b></p>`);
+        }
+        } else if (week[i] == 'сб' || week[i] == 'вс') {
+          document.write(`<p><b>${week[i]}</b></p>`);
+        } else {
+          document.write(`<p>${week[i]}</p>`);
+        }
+  }
+  console.log(week);
+
+  
+
+let arr = ['245', '5688', '458', '355', '325', '789', '4905'];
+
+ arr.forEach((e) => {
+  if (e[0] == '3' || e[0] == '7') {
+    console.log(e);
+  }
+});
